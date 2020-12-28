3edcffdd6e25f14aa08ea7ba19fdb7847334f9a6
mohsen khorasani <pes202020@hotmail.com>
2020-12-28 17:03:57 +0330

    8. Cover Type CRUD

---
+++ BulkyBook.Models/ViewModels/CoverTypeVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class CoverTypeVM
+    {
+        public IEnumerable<CoverType> CoverTypes { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
