206286f2084c8a7d3ae50d6ce51e7f38d396633b
mohsen khorasani <pes202020@hotmail.com>
2021-01-08 20:31:57 +0330

    10. Company and User

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,8 @@ namespace BulkyBook.DataAccess.Repository.IRepository
         ICategoryRepository Category { get; }
         ICoverTypeRepository CoverType { get; }
         IProductRepository Product { get; }
+        IApplicationUserRepository ApplicationUser { get; }
+        ICompanyRepository Company { get; }
 
         ISP_Call SP_Call { get; }
 
